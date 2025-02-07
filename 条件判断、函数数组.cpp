23734f58ecccae419a78b4840dea0357ea398be4
LiuAOSP <1809737680@qq.com>
2025-02-08 07:37:53 +0800

    Add files via upload

---
+++ 条件判断、函数数组.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+//int Add(int x,int y){
+//	return (x + y);
+//}
+
+int main(){
+//	int input = 0;
+//	printf("是否开始崭新的人生（1/0）？");
+//	scanf("%d",&input);
+//	if(input == 1){
+//		printf("欢迎来到理想国！");
+//	}
+//	else{
+//		printf("请加油！");
+//	}
+//    int line =0;
+//    while(line<20000){
+//	
+//    printf("写代码%d......\n",line);
+//    line++;
+//    
+//	} 
+//	if(line >= 20000){
+//		printf("已经写了%d行代码\n",line);
+//	}
+//	else{
+//		printf("请继续加油！");
+//	}
+
+//    int n1 = 0;
+//    int n2 = 0;
+//    printf("请输入两个数字空格隔开：") ;
+//    scanf("%d %d",&n1,&n2);
+//    int sum = Add(n1,n2); 
+//    printf("%d\n",sum);
+//	return 0;
+
+int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//printf("%d",arr[2]) ;
+int i = 0;
+while(i < 10){
+	printf("%d ",arr[i]);
+	i += 1;
+}
+}
+
